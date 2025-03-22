91652b81b75343d6d4c1d9f71d055c87be74a455
Marconik <maminghao0425@qq.com>
2025-03-22 23:57:33 +0800

    Your discription to the commit

--- source/Editor/geometry_nodes/node_curvature.cpp
+++ source/Editor/geometry_nodes/node_curvature.cpp
@@ -11,7 +11,42 @@ typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
 
 namespace Surf
 {
+    float TriAngle(MyMesh::Point A, MyMesh::Point B, MyMesh::Point C);
+    float TriArea(MyMesh::Point A,MyMesh::Point B,MyMesh::Point C);
+    float BaryCen(MyMesh::Point A,MyMesh::Point B,MyMesh::Point C);
 
+    float TriAngle(MyMesh::Point A, MyMesh::Point B, MyMesh::Point C)
+    {
+        auto ea = (B - C).norm();
+        auto eb = (A - B).norm();
+        auto ec = (A - C).norm();
+        auto r = (eb * eb + ec * ec - ea * ea) / (2 * eb * ec);
+        if (std::abs(r) <= 1)
+            return std::acos(r);
+        else
+            return 0;
+    }
+    float TriArea(MyMesh::Point A,MyMesh::Point B,MyMesh::Point C)
+    {
+        auto ea = (B - C).norm();
+        auto eb = (A - B).norm();
+        auto ec = (A - C).norm();
+        auto r = (eb * eb + ec * ec - ea * ea) / (2 * eb * ec);
+        if(std::abs(r) <= 1)
+        {
+            auto s = std::sqrt(1 - r * r);
+            return s * eb * ec / 2;
+        }
+        else
+            return 0;
+    }
+    float BaryCen(MyMesh::Point A,MyMesh::Point B,MyMesh::Point C)
+    {
+        auto G=(A+B+C)/3;
+        auto M1=(A+C)/2;
+        auto M2=(A+B)/2;
+        return TriArea(A,G,M1)+TriArea(A,G,M2);
+    }
 }
 
 
@@ -21,14 +56,66 @@ void compute_mean_curvature(
 {
     // TODO: Implement the mean curvature computation
     //  You need to fill in `mean_curvature`
+    for(auto v_it = omesh.vertices_begin(); v_it != omesh.vertices_end(); ++v_it)
+    {
+        MyMesh::Point v = omesh.point(*v_it);
+        std::vector<MyMesh::Point> node_neighbors;
+        for(auto vv_it = omesh.cvv_iter(*v_it); vv_it.is_valid(); ++vv_it)
+        {
+            node_neighbors.push_back(omesh.point(*vv_it));
+        }
+        auto n = node_neighbors.size();     
+        auto area = Surf::BaryCen(v, node_neighbors[0], node_neighbors[n-1]);
+        auto laplacian=(node_neighbors[0]-v)/std::tan(Surf::TriAngle(node_neighbors[n-1],v,node_neighbors[0]));
+        laplacian += (node_neighbors[n-1] - v) /
+                     std::tan(Surf::TriAngle(node_neighbors[0], v, node_neighbors[n-1]));
+        for(int i = 0;i<n-1;++i)
+        {
+            area += Surf::BaryCen(v, node_neighbors[i], node_neighbors[i+1]);
+            laplacian += (node_neighbors[i] - v) /
+                         std::tan(Surf::TriAngle(
+                             node_neighbors[i + 1], node_neighbors[i], v));
+            laplacian += (node_neighbors[i + 1] - v) /
+                         std::tan(Surf::TriAngle(
+                             node_neighbors[i], node_neighbors[i + 1], v));
+        }
+        auto r=laplacian.norm();
+        r=r/(4*area);
+        mean_curvature.push_back(r);
+    }
 }
 
 void compute_gaussian_curvature(
     const MyMesh& omesh,
     pxr::VtArray<float>& gaussian_curvature)
-{
+{ 
     // TODO: Implement the Gaussian curvature computation
     //  You need to fill in `gaussian_curvature`
+    for(auto v_it = omesh.vertices_begin(); v_it != omesh.vertices_end(); ++v_it)
+    {
+        MyMesh::Point v = omesh.point(*v_it);
+        float omega = 0;
+        float area = 0;
+        std::vector<MyMesh::Point> node_neighbors;
+        for (auto vv_it = omesh.cvv_iter(*v_it); vv_it.is_valid(); ++vv_it)
+        {
+            node_neighbors.push_back(omesh.point(*vv_it));
+        }
+        auto n = node_neighbors.size();
+        for(int i=0;i<n-1;++i)
+        {
+            omega +=
+                Surf::TriAngle(v, node_neighbors[i], node_neighbors[i + 1]);
+            area += Surf::TriArea(v, node_neighbors[i], node_neighbors[i + 1]);
+        }
+        omega += Surf::TriAngle(v, node_neighbors[0], node_neighbors[n - 1]);
+        area += Surf::TriArea(v, node_neighbors[0], node_neighbors[n - 1]);
+        auto K=(2*std::acos(-1)-omega)/area;
+        if(K>=0)
+            gaussian_curvature.push_back(K);
+        else
+            gaussian_curvature.push_back(0);
+    }
 }
 
 NODE_DEF_OPEN_SCOPE
